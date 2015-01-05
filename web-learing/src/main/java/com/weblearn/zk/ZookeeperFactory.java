0dc75038e87bd32810c6d9e6881051c75154449f
rkzhang <zhangrongk@126.com>
2015-01-05 16:10:54 +0800

    zookeeper lock

---
+++ web-learing/src/main/java/com/weblearn/zk/ZookeeperFactory.java
@@ -0,0 +1,35 @@
+package com.weblearn.zk;
+
+import java.io.IOException;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+public class ZookeeperFactory {
+	
+	private static final String CLIENT_PORT = "2181";
+	
+	private static final int CONNECTION_TIMEOUT = 1000;
+
+	private static ZooKeeper zk;
+	
+	static {
+		try {
+			zk = new ZooKeeper("localhost:" + CLIENT_PORT, 
+			        CONNECTION_TIMEOUT, new Watcher() { 
+			            // 监控所有被触发的事件
+			            public void process(WatchedEvent event) { 
+			                System.out.println("已经触发了" + event.getType() + "事件！"); 
+			                System.out.println("modify path --- " + event.getPath()); 
+			            } 
+			        });
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	public static ZooKeeper achieveZkClient() {
+		 return zk;
+	}
+}
